2122a926e5282ee084095279024fd1e9a606234e
Guilherme Moraes Furtado <guilhermefurtado78@gmail.com>
2022-12-30 10:10:05 -0300

    adicionado o URL do local host

--- app/src/main/java/com/sigtecinformatica/appteste/Utils/Apis.java
+++ app/src/main/java/com/sigtecinformatica/appteste/Utils/Apis.java
@@ -2,7 +2,8 @@ package com.sigtecinformatica.appteste.Utils;
 
 public class Apis {
 
-    public static final String URL_001 = "http://192.168.1.175:8080/";
+    //public static final String URL_001 = "http://192.168.1.175:8080/";
+    public static final String URL_001 = "http://127.0.0.1:8080/";
 
     public static PersonaService getPersonaService(){
         return Cliente.getCliente(URL_001).create(PersonaService.class);
